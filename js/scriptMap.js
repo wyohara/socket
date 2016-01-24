87aad82c79813c53c2404daae9dbcb518a53b48c
Willian Ohara <wyohara@gmail.com>
2016-01-24 18:54:06 -0200

    teste de obter marcadores com delay
    teste de obter marcadores com delay

--- js/scriptMap.js
+++ js/scriptMap.js
@@ -1,15 +1,18 @@
-var resposta;
-var socket;
+var resposta='';
 var host= 'ws://187.14.231.115:8889';
+var socket;
 var samp= document.createElement('samp');
 
 //NOTE o primeiro momento ao carregar a pagina é criado o mapa:
 (function() {
   window.onload = function() {
     // Geracao do mapa
-    newMap(-22.6086,-43.7128,10);
     //NOTE envia o sinal com a localização e é gerado a resposta para mostrar os pontos proximos
-    sendLocal(-22.6086,-43.7128);
+    window.setTimeout(function() {
+      sendLocal(-22.6086,-43.7128);
+      return;
+    },500);
+    newMap(-22.6086,-43.7128,10);
     //NOTE é gerado a geolocalização verdadeira e criado um novo mapa e novos marcadores
     getLocation()
   };
@@ -17,7 +20,6 @@ var samp= document.createElement('samp');
 
 
 function sendLocal(lat,lon){
-  socket = null;
   var print  = function (message) {
     samp.innerHTML = message + '\n';
     output.appendChild(samp);
@@ -25,21 +27,21 @@ function sendLocal(lat,lon){
   };
   socket = new WebSocket(host);
   socket.onopen = function () {
-    //console.log('connection is opened');
-    //var envio='{"action": "createMap","latitude": '+lat+',"longitude": '+lon+'}';
-    //console.log("Sinal Enviado"+envio);
     var envio='{"action": "getMarkers","latitude": '+lat+',"longitude": '+lon+'}';
-    console.log("Sinal Enviado"+envio);
     socket.send(envio);
+    console.log("Sinal Enviado: "+envio);
     return;
   };
+  //NOTE resposta da conexao
   socket.onmessage = function (msg) {
     document.getElementById("resp").innerHTML=msg.data;
     resposta=JSON.parse(msg.data);
-    console.log("resposta"+msg.data);
+    console.log("Resposta Original "+ msg.data);
+    console.log("Resposta parsed "+ resposta.markers.length);
     return;
   };
 }
+
 
 function getLocation() {
   var lat, lon;
@@ -75,8 +77,35 @@ function newMap(lat,lon,zoom){
     title: "Estou Aqui",
     visible:true,
   });
+
+  if(resposta.action=="sendMarkers"){
+    for (var i = 0, length = resposta.markers.length; i < length; i++) {
+      var data = resposta.markers[i];
+      latLng = new google.maps.LatLng(data.p_lat,data.p_lon);
+      // inserindo marcador no mapa
+      var marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        icon:"imagem/local.png",
+        title: 'ponto',
+      });
+    }
+  }
+
+  /* NOTE infowindow indicando a localização
   var infowindow = new google.maps.InfoWindow({
     content: "Estou aqui",
   });
-    infowindow.open(map, marker);
-}
+    infowindow.open(map, marker);*/
+    //NOTE criando event quando mudara  posição do center usando drag
+    map.addListener('dragend', function() {
+       window.setTimeout(function() {
+         var position=String(map.getCenter());
+         position=position.replace("(","").replace(")","").split(",");
+         //console.log('local:'+map.getCenter());
+         sendLocal(position[0],position[1]);
+         return;
+       }, 500);
+     });
+   }
