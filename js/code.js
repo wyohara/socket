4dd2f357fa37a0e895dca26470c405f1a31a20d5
Willian Ohara <wyohara@gmail.com>
2016-01-25 18:12:11 -0200

    Nova correção de Sockets

--- js/code.js
+++ js/code.js
@@ -1,157 +1,75 @@
 var resposta='';
+var cont=0;
 var host= 'ws://138.204.212.65:8889';
-var socket;
-<<<<<<< HEAD
+//var host= 'ws://127.0.0.1:8889';
+var socket = null;
+var map = null;
 
-//NOTE o primeiro momento ao carregar a pagina é criado o mapa:
-  window.onload = function() {
-    //NOTE criando a conexão websocket
-    conectar();
-    enviar('{"action": "getMarkers","latitude": '+-22.6086+',"longitude": '+-43.7128+'}');
-    newMap(-22.6086,-43.7128,10)
-    // Geracao do mapa
-    //NOTE envia o sinal com a localização e é gerado a resposta para mostrar os pontos proximos
-    //sendLocal(-22.6086,-43.7128,10);
-=======
-var map, latLng, marker;
-
-//NOTE o primeiro momento ao carregar a pagina é criado o mapa:
-//Isso não é uma função
 window.onload = function() {
-  //NOTE criando a conexão websocket
-  conectar();
-  
-  enviar('{"action": "getMarkers","latitude": '+-22.6086+',"longitude": '+-43.7128+'}');
-  
-  newMap(-22.6086,-43.7128,10);
-  // Geracao do mapa
-  //NOTE envia o sinal com a localização e é gerado a resposta para mostrar os pontos proximos
-  //sendLocal(-22.6086,-43.7128,10);
->>>>>>> origin/master
-
-  //NOTE é gerado a geolocalização verdadeira e criado um novo mapa e novos marcadores
-  //getLocation(15)
-};
-
-<<<<<<< HEAD
-    //NOTE é gerado a geolocalização verdadeira e criado um novo mapa e novos marcadores
-    //getLocation(15)
-  };
-
-=======
->>>>>>> origin/master
-function conectar(){
+  console.log("conectando");
   socket = new WebSocket(host);
-  
-  //Isso não é uma função
   socket.onopen = function () {
     console.log("Conectado.");
-<<<<<<< HEAD
     return;
   };
-}
 
-function enviar(mensagem){
-  socket.onopen = function () {
-    socket.send(mensagem);
-    console.log("Sinal Enviado: "+mensagem);
-=======
-    
-    return;
-  };
-  //Isso não é uma função
-  //É como se fosse um trigger, acontecerá quando receber mensagem. não precisa ser chamado.
-  socket.onmessage = function (msg) {
-    document.getElementById("resp").innerHTML=msg.data;
-    resposta=JSON.parse(msg.data);
-    console.log("Resposta Original "+ msg.data);
-    console.log("Resposta parsed "+ resposta.markers.length);
-    
->>>>>>> origin/master
-    return;
-  };
-}
-
-//Isso é uma função
-function enviar(mensagem){
-  socket.send(mensagem);
-  
-  return;
-}
-
-function newMap(lat,lon,zoom){
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: new google.maps.LatLng(lat,lon),
-    zoom:zoom,
+  console.log("gerar mapa");
+  map = new google.maps.Map(document.getElementById("map"),{
+    center: new google.maps.LatLng(-22.6086,-43.7128),
+    zoom:10,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
-  latLng = new google.maps.LatLng(lat,lon);
-  marker = new google.maps.Marker({
+  var latLng = new google.maps.LatLng(-22.6086,-43.7128);
+  console.log("gerar centro");
+  var marker = new google.maps.Marker({
     position: latLng,
     map: map,
     icon:"imagem/point2.png",
     animation: google.maps.Animation.DROP,
     title: "Estou Aqui",
-    visible:true
+    visible:true,
   });
-<<<<<<< HEAD
-  //carregando resposta
-  socket.onmessage = function (msg) {
-    document.getElementById("resp").innerHTML=msg.data;
-    resposta=JSON.parse(msg.data);
-    if(resposta.action=="sendMarkers"){
-      for (var i = 0, length = resposta.markers.length; i < length; i++) {
-        var data = resposta.markers[i];
-        latLng = new google.maps.LatLng(data.p_lat,data.p_lon);
-        // inserindo marcador no mapa
-        var marker = new google.maps.Marker({
-          position: latLng,
-          map: map,
-          animation: google.maps.Animation.DROP,
-          icon:"imagem/local.png",
-          title: 'ponto',
-        });
-      }
-=======
 
-  if(resposta != '' && resposta.action=="sendMarkers"){
-    for (var i = 0, length = resposta.markers.length; i < length; i++) {
-      var data = resposta.markers[i];
-      latLng = new google.maps.LatLng(data.p_lat,data.p_lon);
-      // inserindo marcador no mapa
-      marker = new google.maps.Marker({
-        position: latLng,
-        map: map,
-        animation: google.maps.Animation.DROP,
-        icon:"imagem/local.png",
-        title: 'ponto'
-      });
->>>>>>> origin/master
-    }
+  socket.onopen = function () {
+    obter();
+    return;
   };
 
-<<<<<<< HEAD
+
   //NOTE criando event quando mudara  posição do center usando drag
-    map.addListener('dragend', function() {
-         var position=String(map.getCenter());
-         position=position.replace("(","").replace(")","").split(",");
-         //console.log('local:'+map.getCenter());socket = new WebSocket(host);
-         socket=new WebSocket(host)
-         enviar('{"action": "getMarkers","latitude": '+position[0]+',"longitude": '+position[1]+'}');
-     });
-=======
-    //NOTE criando event quando mudara  posição do center usando drag
   map.addListener('dragend', function() {
-     window.setTimeout(function() {
-       var position=String(map.getCenter());
-       position=position.replace("(","").replace(")","").split(",");
-       //console.log('local:'+map.getCenter());
-       enviar('{"action": "getMarkers","latitude": '+position[0]+',"longitude": '+position[1]+'}');
-       console.log("outro sinal");
-       
-       return;
-     }, 500);
-   });
->>>>>>> origin/master
-   }
+    cont=cont+1;
+    console.log("Dragened, Conexão: "+cont);
+    obter();
+  });
+
+};
+
+function obter(){
+  //obtem novo center apos o frag
+  var position=String(map.getCenter());
+  position=position.replace("(","").replace(")","").split(",");
+    socket.send('{"action": "getMarkers","latitude": '+position[0]+',"longitude": '+position[1]+'}');
+    console.log("Enviado conexão: "+cont);
+    //console.log('{"action": "getMarkers","latitude": '+position[0]+',"longitude": '+position[1]+'}');
+    socket.onmessage = function (msg) {
+
+      resposta=JSON.parse(msg.data);
+      console.log("Recebida a resposta da conexão: "+cont);
+
+      if(resposta.action=="sendMarkers"){
+        for (var i = 0, length = resposta.markers.length; i < length; i++) {
+          var data = resposta.markers[i];
+          latLng = new google.maps.LatLng(data.p_lat,data.p_lon);
+          // inserindo marcador no mapa
+          var marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+            icon:"imagem/local.png",
+            title: data.p_nome,
+          });
+        }
+      };
+    }
+}
--- js/scriptMap.js
+++ js/scriptMap.js
@@ -1,73 +1,63 @@
+/**
+ *  Iniciando variaveis
+ */
 var resposta='';
-var host= 'ws://138.204.212.65:8889';
-var socket;
-var samp= document.createElement('samp');
-
-//NOTE o primeiro momento ao carregar a pagina é criado o mapa:
-(function() {
-  window.onload = function() {
-    // Geracao do mapa
-    //NOTE envia o sinal com a localização e é gerado a resposta para mostrar os pontos proximos
-      sendLocal(-22.6086,-43.7128,10);
-      alert("a");
-      newMap(-22.6086,-43.7128,10);
-    //NOTE é gerado a geolocalização verdadeira e criado um novo mapa e novos marcadores
-    getLocation(15)
-  };
-})();
+//var host= 'ws://138.204.212.65:8889';
+var host= 'ws://127.0.0.1:8889';
+var socket = null;
+var map = null;
 
 
-function sendLocal(lat,lon,zoom){
-  var print  = function (message) {
-    samp.innerHTML = message + '\n';
-    output.appendChild(samp);
-    return;
-  };
+/**
+ *    Criando funções
+ */
+function conectar(){  //função para conectar ao socket
   socket = new WebSocket(host);
   socket.onopen = function () {
-    console.log("enviando");
-    var envio='{"action": "getMarkers","latitude": '+lat+',"longitude": '+lon+'}';
-    socket.send(envio);
-    console.log("Sinal Enviado: "+envio);
+    console.log("Conectado.");
     return;
   };
-  //NOTE resposta da conexao
+
   socket.onmessage = function (msg) {
-    document.getElementById("resp").innerHTML=msg.data;
+    console.log("recebido: "+msg.data);
     resposta=JSON.parse(msg.data);
-    console.log("Resposta Original "+ msg.data);
-    console.log("Resposta parsed "+ resposta.markers.length);
-    return resposta;
+
+    //Identifica mensagem recebida
+    switch (resposta.action) {
+      case 'sendMarkers':
+        for (var i = 0; i < resposta.markers.length; i++) {
+          var respM = resposta.markers[i];
+          var latLng = new google.maps.LatLng(respM.lat,respM.lat);
+          // inserindo marcador no mapa
+          var marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+            icon:"imagem/local.png",
+            title: data.p_nome,
+          });
+        }
+        break;
+      default:
+        console.log('Mensagem não identificada:');
+        console.log(resposta);
+    }
   };
-
 }
 
-/*
-function getLocation(zoom) {
-  var lat, lon;
-		if (navigator.geolocation) {
-				navigator.geolocation.getCurrentPosition(showPosition);
-		} else {
-				console.log('Navegar não suporta Geolocalização');
-		}
-
-    //NOTE segunda parte da função, onde se obtem a coordenada
-    function showPosition(position) {
-      lat = position.coords.latitude,
-      lon = position.coords.longitude;
-      //sendLocal(lat,lon,zoom);
-	}
+function enviar(mensagem){  //função de envio de resposta
+  socket.send(mensagem);
+  console.log("Sinal Enviado: "+mensagem);
 }
-*/
-function newMap(lat,lon,zoom){
-  var map = new google.maps.Map(document.getElementById("map"), {
+
+function newMap(lat,lon,zoom){  //função de criação de mapa
+  map = new google.maps.Map(document.getElementById("map"),{
     center: new google.maps.LatLng(lat,lon),
     zoom:zoom,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
-  latLng = new google.maps.LatLng(lat,lon);
-  // inserindo marcador no mapa
+  var latLng = new google.maps.LatLng(lat,lon);
+
   var marker = new google.maps.Marker({
     position: latLng,
     map: map,
@@ -77,34 +67,26 @@ function newMap(lat,lon,zoom){
     visible:true,
   });
 
-  if(resposta.action=="sendMarkers"){
-    for (var i = 0, length = resposta.markers.length; i < length; i++) {
-      var data = resposta.markers[i];
-      latLng = new google.maps.LatLng(data.p_lat,data.p_lon);
-      // inserindo marcador no mapa
-      var marker = new google.maps.Marker({
-        position: latLng,
-        map: map,
-        animation: google.maps.Animation.DROP,
-        icon:"imagem/local.png",
-        title: 'ponto',
-      });
-    }
-  }
-
-  /* NOTE infowindow indicando a localização
-  var infowindow = new google.maps.InfoWindow({
-    content: "Estou aqui",
+  //NOTE criando event quando mudara  posição do center usando drag
+  map.addListener('dragend', function() {
+    //obtem novo center apos o frag
+    var position=String(map.getCenter());
+    position=position.replace("(","").replace(")","").split(",");
+    //Envia para o socket a nova pos
+    enviar('{"action": "getMarkers","latitude": '+position[0]+',"longitude": '+position[1]+'}');
   });
-    infowindow.open(map, marker);*/
-    //NOTE criando event quando mudara  posição do center usando drag
-    map.addListener('dragend', function() {
-       window.setTimeout(function() {
-         var position=String(map.getCenter());
-         position=position.replace("(","").replace(")","").split(",");
-         //console.log('local:'+map.getCenter());
-         //sendLocal(position[0],position[1]);
-         return;
-       }, 500);
-     });
-   }
+}
+
+
+
+//NOTE Funcção para ser chamada ao fim do carregamento da página
+window.onload = function() {
+  //NOTE conecta com o servidor websocket
+  conectar();
+
+  // NOTE: Envia uma mensagem
+  enviar('{"action": "getMarkers","latitude": '+-22.6086+',"longitude": '+-43.7128+'}');
+
+  // NOTE: Gera mapa
+  newMap(-22.6086,-43.7128,10);
+};
